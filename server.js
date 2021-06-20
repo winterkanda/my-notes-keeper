ce65507986d30b95ac12a972948cb75cb26a0b0d
Kiran Kanda <kkanda@dons.usfca.edu>
2021-06-19 19:57:51 -0700

    added api route to notes

--- server.js
+++ server.js
@@ -3,27 +3,19 @@ const fs = require('fs');
 const path = require('path');
 
 const express = require('express');
-const database = require("./db/db.json");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+// middleware to serve as a front-end as well as JSON data since we created a server
+app.use(express.static('public'));
 
 // parse incoming string or array data
 app.use(express.urlencoded({extended: true}));
 // parse incoming JSON data
 app.use(express.json());
 
-// middleware to serve as a front-end as well as JSON data since we created a server
-app.use(express.static('public'));
-
-//HTML Routes, to load page
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
-})
-
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
-})
+const database = require("./db/db.json");
 
 // GET to read db.json file
 app.get('/api/notes', (req,res) => {
@@ -34,6 +26,17 @@ app.get('/api/notes', (req,res) => {
 app.get('/', (req,res) =>{
     res.sendFile(path.join(_dirname, '/public/index.html'));
 });
+
+//this grabs notes
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+//this routh links to the index.html
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
 
 app.listen(3001, () => {
     console.log(`API server now on port 3001!`);
